a7cdb9c4f6e09eee9fd111a6a07e2bc095764df8
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2026-03-12 23:29:53 +0100

    fix: show also debug messages in console
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,15 +7,15 @@ use tracing_subscriber::fmt::writer::MakeWriterExt;
 pub fn init() {
     // Configure logging
     if env::var("ENV") != Ok("testing".to_string()) {
-        let stdout = std::io::stdout;
+        let stdout = std::io::stdout.with_filter(|meta| meta.target() == "app");
         let debug_file = RollingFileAppender::builder()
             .rotation(Rotation::DAILY)
-            .filename_prefix("all")
+            .filename_prefix("info")
             .filename_suffix("log")
             .max_log_files(5)
             .build("./logs")
-            .expect("initializing rolling debug_file appender failed")
-            .with_filter(|meta| meta.target() == "app");
+            .expect("initializing rolling info_file appender failed")
+            .with_max_level(tracing::Level::INFO);
         let error_file = RollingFileAppender::builder()
             .rotation(Rotation::DAILY)
             .filename_prefix("error")
@@ -30,6 +30,7 @@ pub fn init() {
             .compact()
             .with_writer(writer)
             .with_ansi(false)
+            .with_max_level(tracing::Level::DEBUG)
             .init();
     }
 
